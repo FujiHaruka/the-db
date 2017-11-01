e2326d40cbff10ae15f80048220120ee0b04b3e3
okunishinishi <okunishinishi@gmail.com>
2017-11-01 13:23:12 +0900

    Add db logs

--- doc/api/api.md
+++ doc/api/api.md
@@ -1,4 +1,4 @@
-# the-db@7.8.6
+# the-db@7.8.7
 
 DB for the-framework
 
---
+++ test/LogResourceTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for LogResource.
+ * Runs with mocha.
+ */
+'use strict'
+
+const LogResource = require('../lib/resources/LogResource')
+const { ok, equal } = require('assert')
+
+describe('log-resource', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
