984d4b0c69317b54f65d6b7003bec7491a58180c
FujiHaruka <fujiharuka@realglobe.jp>
2018-04-26 13:50:44 +0900

    exportImportを100倍高速にした

--- test/exportImportMixTest.js
+++ test/exportImportMixTest.js
@@ -4,17 +4,54 @@
  */
 'use strict'
 
-const exportImportMix = require('../lib/mixins/exportImportMix')
+const TheDB = require('../lib/TheDB')
+// const exportImportMix = require('../lib/mixins/exportImportMix')
 const { ok, equal } = require('assert')
+const {TheResource} = require('the-resource-base')
 
-describe('export-import-mix', () => {
-  before(() => {
+describe('export-import-mix', function () {
+  this.timeout(10000)
+  let db
+
+  before(async () => {
+    db = new TheDB({
+      env: {
+        dialect: 'sqlite',
+        storage: `${__dirname}/../tmp/exp-imp-test/test.db`
+      },
+    })
+
+    class UserResource extends TheResource {
+      static get policy () {
+        return {
+          name: {type: 'STRING'},
+        }
+      }
+    }
+
+    db.load(UserResource, 'User')
   })
 
-  after(() => {
+  after(async () => {
+    await db.drop()
+    await db.close()
   })
 
-  it('Do test', () => {
+  it('Do test', async () => {
+    const {User} = db.resources
+
+    const DATA_COUNT = 1234
+
+    const users = new Array(DATA_COUNT).fill(null).map((_, i) => ({name: String(i)}))
+    await User.createBulk(users)
+
+    const dataDir = `${__dirname}/../tmp/exp-imp-test/data`
+
+    await db.export(dataDir)
+    await db.drop()
+    await db.import(dataDir)
+
+    equal(await User.count(), DATA_COUNT)
 
   })
 })
